a752663bd4444620489820112f27cf62dc616396
shivanikalva3-max <shivanikalva3@gmail.com>
2026-09-10 20:04:34 +0530

    Create palindrome number.py

---
+++ palindrome number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        reverse=0
+        y=x
+        while(x>0):
+            digit=x%10
+            reverse=reverse*10+digit
+            x=x//10
+        if(y==reverse):
+            return True
+        else:
+            return False
+        
+
+            
