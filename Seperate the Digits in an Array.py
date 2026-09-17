132bd84d8ce244c93146f96e3fe18653a27cf0a7
shivanikalva3-max <shivanikalva3@gmail.com>
2026-09-17 22:17:20 +0530

    Create Seperate the Digits in an Array.py

---
+++ Seperate the Digits in an Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        ans = []
+        for num in nums:
+            for digit in str(num):
+                ans.append(int(digit))
+        return ans
+
