bb86d5a1be8025b77211c4e787437edd5e270d43
shivanikalva3-max <shivanikalva3@gmail.com>
2026-09-18 21:30:22 +0530

    Create Sqrt(x).py

---
+++ Sqrt(x).py
@@ -0,0 +1,8 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x==0 or x==1:
+            return x
+        for i in range(x+1):
+            if i*i>x:
+                return i-1
+       
